086caef7bc33176b016144df6227b37f15024ce1
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-14 03:47:18 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+  end
+   
+end
