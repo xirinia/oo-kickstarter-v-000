4a632e891b550e7c137c6dc0e266a4370325b7fa
Anaxirinia Jimenez <anax131@gmail.com>
2018-01-14 05:12:23 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -11,9 +11,6 @@ class Project
   def add_backer(backer)
     @backers << backer
     backer.backed_projects << self
-  #  self.add_backer(self)
-
-    #we want to push project into backed-project
   end
 
 end
